e1cd2337ddb03f1b66a6c736256040c77ff85931
hivetestqa <testqa@thehive.ai>
2022-11-24 15:50:55 +0530

    patch for closing updates tab SVG file format

--- MensioWebSanity/src/test/java/StepDefinition/login.java
+++ MensioWebSanity/src/test/java/StepDefinition/login.java
@@ -43,6 +43,7 @@ public void user_is_navigated_to_home_screen() {
 	Assert.assertEquals(true, HomeElement.isDisplayed());
 	driver.findElement(By.className("jss121")).click();
 	System.out.println("User navigated to home screen");
+	driver.findElement(By.xpath("//*[name()='svg']//*[local-name()='g']//*[local-name()='path' and @d='M13 1L1 13M1 1l12 12']")).click(); // line for closing updates tab
 }
 
 @And("^take screenshot$")
